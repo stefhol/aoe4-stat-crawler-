4132d18a9eaae98eff5a263f1e756afd9dacd300
Stefan <hoef.stef@pm.me>
2021-11-03 15:21:21 +0100

    add command line interface

--- crawler/src/main.rs
+++ crawler/src/main.rs
@@ -3,6 +3,7 @@ use std::{collections::HashMap, net::SocketAddr, thread};
 mod actor;
 mod model;
 use anyhow::Error;
+use clap::{App, Arg};
 use log::{error, info};
 use reqwest::Client;
 
@@ -15,23 +16,45 @@ use crate::model::{
 async fn main() -> Result<(), Error> {
     log4rs::init_file("config/log4rs.yml", Default::default()).unwrap();
 
-    let addr: SocketAddr = "[::1]:50051".parse()?;
     let client = reqwest::Client::new();
 
-    info!(
-        "Starting server at localip {}:{} ",
-        local_ip_address::local_ip().unwrap().to_string(),
-        addr.port()
-    );
-    //first request starting at page 1
+    let matches = App::new("Age of Empires Leaderboard Crawler")
+        .version("1.0")
+        .author("Stefan Hoefler")
+        .arg(Arg::with_name("DB String")
+                .short("db")
+                .long("database")
+                .value_names(&["Connection String"])
+                .help("provides a custom connection string if non provided uses DATABASE_URL out of env. Only supporting PostgresSql")
+        )
+        .arg(
+            Arg::with_name("team_size")
+                .value_names(&["team_size"])
+                .possible_values(&["1v1", "2v2", "3v3", "4v4"])
+                .max_values(1)
+                .required(true)
+                .short("ts")
+                .long("team_size")
+                .help("Sets which team size to save")
+                .takes_value(true),
+        )
+        .get_matches();
 
-    crawl_aoe4_every_leaderboard(TeamSize::T1v1,client).await;
+   let team_size:TeamSize = match matches.value_of("team_size") {
+       Some("1v1") => TeamSize::T1v1,
+       Some("2v2") => TeamSize::T2v2,
+       Some("3v3") => TeamSize::T3v3,
+       Some("4v4") => TeamSize::T4v4,
+       _=> panic!("Team Size is not correct")
+   };
+
+    crawl_aoe4_every_leaderboard(team_size, client).await;
     Ok(())
 }
 
-async fn crawl_aoe4_every_leaderboard(team_size: TeamSize,client:Client) {
+async fn crawl_aoe4_every_leaderboard(team_size: TeamSize, client: Client) {
     //Inital Request has to be performed to know how many sites there are
-    let inital_request = crawl_aoe4_singel_leaderboard(1, &team_size,&client).await;
+    let inital_request = crawl_aoe4_singel_leaderboard(1, &team_size, &client).await;
     log_status(&inital_request, &1);
     if let Ok(leaderboard) = inital_request {
         add_leaderboard_page_to_db("", &leaderboard);
@@ -48,7 +71,7 @@ async fn crawl_aoe4_every_leaderboard(team_size: TeamSize,client:Client) {
         for page in 2..page_number + 1 {
             //wait before requesting to not spam the server with too much at the same time
             thread::sleep(time::Duration::from_secs(2));
-            let request = crawl_aoe4_singel_leaderboard(page, &team_size,&client).await;
+            let request = crawl_aoe4_singel_leaderboard(page, &team_size, &client).await;
             log_status(&request, &page);
             if let Ok(leaderboard) = request {
                 add_leaderboard_page_to_db("", &leaderboard);
@@ -71,7 +94,7 @@ fn log_status(leaderboard: &Result<Leaderboard, Error>, page: &u32) {
 async fn crawl_aoe4_singel_leaderboard(
     page: u32,
     team_size: &TeamSize,
-    client:&Client
+    client: &Client,
 ) -> Result<Leaderboard, Error> {
     let mut request = model::Request::AgeOfEmpiresLeaderboardRequest::new(
         page,
